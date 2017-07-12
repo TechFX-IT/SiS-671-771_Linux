0f8373a0f0affb71208a01d0bfb4ffe07e26d276
TechFX <modderwindowsandroid@gmail.com>
2017-07-12 15:23:44 +0000

    Add files via upload
    Script that installs your drivers.

---
+++ SiS 671-771 drivers.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+#Start showing
+echo "Welcome to the installation of SiS 671-771 GPUs' drivers."
+echo ""
+echo "The script may ask you for root permissions, so you'll type your user account's password to complete the process succesfully."
+echo ""
+echo "Immediatley after the installation the PC will reboot, so don't worry, it's all normal."
+echo ""
+echo "Let's begin."
+echo ""
+#Installing git and cloning files
+sudo apt-get install git
+git clone https://github.com/TechFX-IT/SiS-671-771_Linux $HOME/SiS\ drivers
+#CD into the folder containing the files
+cd $HOME/SiS\ drivers
+#Unpacking
+tar -xvzf $HOME/SiS\ drivers/sis64.tar.gz
+#Moving driver to the folder
+sudo mv -v $HOME/SiS\ drivers/sisimedia_drv.* /usr/lib/xorg/modules/drivers
+#Deleting dangerous files
+sudo rm -v /etc/X11/xorg.conf
+sudo rm -v ~/.config/monitors.xml
+#Moving xorg.conf
+sudo mv -v $HOME/SiS\ drivers/xorg.conf /etc/X11
+#Reboot
+echo ""
+echo ""
+echo "Rebooting now..."
+echo ""
+echo ""
+sudo reboot
